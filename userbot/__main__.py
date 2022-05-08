e1be79f27af78c87e34c9b5e5a4a14b19abf9ef5
dodikridwan <30350392+dodikridwan@users.noreply.github.com>
2022-05-08 21:13:31 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -43,7 +43,7 @@ if not BOT_TOKEN:
 LOGS.info(
     f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/NastySupportt")
 LOGS.info(
-    f"✨Kyy-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"✨Haico-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
